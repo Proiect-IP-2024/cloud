1c9960a239336d13d69958fc873da667da823e4f
Alin Harda <harda.alin.c@gmail.com>
2024-05-13 19:28:44 +0300

    added login and refreshToken

--- src/auth.ts
+++ src/auth.ts
@@ -84,6 +84,102 @@ app.post("/user/createUser", async (req: Request, res: Response) => {
   }
 });
 
+app.post("/user/login", async (req: Request, res: Response) => {
+  try {
+    const userData: User | null = req.body?.userData;
+
+    //Verify the request data is ok
+    if (!(userData && userData?.password && userData?.email)) {
+      return res.status(400).send("Invalid data!");
+    }
+
+    pool.getConnection((error: any, conn) => {
+      if (error) {
+        return res.status(500).send("Connection error");
+      }
+
+      conn.query(
+        `SELECT * FROM Users WHERE email = ? `,
+        [userData.email],
+        async (err, rows) => {
+          if (err) {
+            conn.release(); // Release connection in case of error
+            console.error(err);
+            return res.sendStatus(500);
+          }
+
+          if (rows[0] === undefined) {
+            conn.release(); // Release connection if user already exists
+            return res.status(500).send("User not found");
+          }
+
+          const user = rows[0];
+
+          //Check if password is correct
+          if (!(await bcrypt.compare(userData.password, user.password_hash))) {
+            conn.release(); // Release connection if user already exists
+            return res.status(500).send("Invalid password");
+          }
+
+          const token = jwt.sign(
+            { id: user.id, email: user.email },
+            process.env.JWT_SECRET || "secret",
+            {
+              expiresIn: "1h",
+            }
+          );
+
+          conn.release(); // Release connection after second query
+          return res.status(200).send({ token });
+        }
+      );
+    });
+  } catch (e) {
+    console.error(e);
+    return res.sendStatus(500);
+  }
+});
+
+app.get("/user/refreshToken", async (req: Request, res: Response) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(400).send("Invalid token");
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET || "secret", (err, user) => {
+      if (err) {
+        if (err.name !== "TokenExpiredError") {
+          return res.status(403).send("Invalid token");
+        }
+
+        const newToken = jwt.sign(
+          { id: user.id, email: user.email },
+          process.env.JWT_SECRET || "secret",
+          {
+            expiresIn: "1h",
+          }
+        );
+        return res.status(200).send({ token: newToken });
+      }
+
+      const newToken = jwt.sign(
+        { id: user.id, email: user.email },
+        process.env.JWT_SECRET || "secret",
+        {
+          expiresIn: "1h",
+        }
+      );
+
+      return res.status(200).send({ token: newToken });
+    });
+  } catch (e) {
+    console.error(e);
+    return res.sendStatus(500);
+  }
+});
+
 app.listen(PORT, () => {
   return console.log(`\nAUTH server is listening at PORT: ${PORT}`);
 });
