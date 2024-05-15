afcb3a05c61390b9e024708f907585e10b99c558
Alin Harda <harda.alin.c@gmail.com>
2024-05-15 13:37:41 +0300

    added cors policy

--- src/auth.ts
+++ src/auth.ts
@@ -6,6 +6,13 @@ import { User } from "./modules/interfaces";
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(express.json());
 
 const PORT = process.env.AUTH_SERVER_PORT || 3000;
